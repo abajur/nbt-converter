d4744d0d5348a4d9bc33fe01ca72c3da1446728c
Abajur <abajur@users.noreply.github.com>
2021-02-03 20:03:38 -0300

    read palettes from files

--- nbt_converter.py
+++ nbt_converter.py
@@ -10,9 +10,25 @@ if len(sys.argv) != 2:
     print(f'usage: {sys.argv[0]} <file>')
     sys.exit(1)
 
-palette = nbtlib.load(sys.argv[1]).root['palette']
-for block in palette:
-    print(block.snbt())
+palette_1_12_2 = []
+with open('palette-1.12.2.txt') as f:
+    for line in f.readlines():
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+        palette_1_12_2.append(nbtlib.parse_nbt(line))
+
+palette_1_16_5 = []
+with open('palette-1.16.5.txt') as f:
+    for line in f.readlines():
+        line = line.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
+        palette_1_16_5.append(nbtlib.parse_nbt(line))
+
+assert len(palette_1_12_2) == len(palette_1_16_5)
+
+print('success')
 
 '''
 # 1.16.5
